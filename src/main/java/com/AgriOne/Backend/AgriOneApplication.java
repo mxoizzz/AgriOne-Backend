13b03b59c311a290da4585e4b10e84eaa49fac6d
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2025-09-21 06:45:34 +0530

    Initial commit – Spring Boot project setup

---
+++ src/main/java/com/AgriOne/Backend/AgriOneApplication.java
@@ -0,0 +1,13 @@
+package com.AgriOne.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriOneApplication.class, args);
+	}
+
+}
